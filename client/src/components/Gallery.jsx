26632896a5b4795d5d4790c074cba024b8bd7a79
antran1245 <antran1245@gmail.com>
2022-10-25 08:57:48 -0700

    card component for gallery

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,11 +1,9 @@
-import { Row, Col, Card } from "react-bootstrap"
-import bookmarkFull from '../assets/icon-bookmark-full.svg'
-import bookmarkEmpty from '../assets/icon-bookmark-empty.svg'
-import categoryMovie from '../assets/icon-category-movie.svg'
-import categoryTV from '../assets/icon-category-tv.svg'
+import { useState } from "react"
+import { Row } from "react-bootstrap"
+import GalleryCard from "./GalleryCard";
 import '../sass/gallery.scss'
 
-export default function Gallery({searching, gallery, imagesSmall,  bookmark}) {
+export default function Gallery({searching, gallery, imagesSmall, bookmark}) {
     return(
         <Row id="gallery">
             <Row>
@@ -13,30 +11,7 @@ export default function Gallery({searching, gallery, imagesSmall,  bookmark}) {
             </Row>
             <Row id="gallery-content">
                 {gallery.map((item, index) => {
-                    return <Col xs={6} sm={4} md={4} lg={3} xl={2} key={index}>
-                        <Card className='text-white border-0'>
-                            <Card.Img variant="top" src={require("../assets/thumbnails"+imagesSmall[index])} alt={item.title}/>
-                            <Card.ImgOverlay>
-                                <Card.Text className="bookmark" onClick={() => bookmark(index)}>
-                                    <img src={item.isBookmarked? bookmarkFull : bookmarkEmpty} alt="bookmark"/>
-                                </Card.Text>
-                            </Card.ImgOverlay>
-                            <Card.Body>
-                                <Card.Text className="card-info">
-                                    <span className="info">
-                                        <span>2019</span>
-                                        <span>&#183;</span>
-                                        <span className='catergory'>
-                                            <img src={item.category === "Movie"? categoryMovie : categoryTV} alt="category"/> {item.category === "Movie"? "Movie" : "TV Series"}
-                                        </span>
-                                        <span>&#183;</span>
-                                        <span>{item.rating}</span>
-                                    </span>
-                                    <span className="title">{item.title}</span>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                    return <GalleryCard item={item} image={imagesSmall[index]} bookmark={bookmark} index={index} key={index}/>
                 })}
             </Row>
         </Row>
---
+++ client/src/components/GalleryCard.jsx
@@ -0,0 +1,35 @@
+import { Col, Card } from 'react-bootstrap'
+import bookmarkFull from '../assets/icon-bookmark-full.svg'
+import bookmarkEmpty from '../assets/icon-bookmark-empty.svg'
+import categoryMovie from '../assets/icon-category-movie.svg'
+import categoryTV from '../assets/icon-category-tv.svg'
+import '../sass/gallerycard.scss'
+
+export default function GalleryCard({item, image, bookmark, index}) {
+    return (
+        <Col xs={6} sm={4} md={4} lg={3} xl={2}>
+            <Card className='text-white border-0'>
+                <Card.Img variant="top" src={require("../assets/thumbnails"+image)} alt={item.title}/>
+                <Card.ImgOverlay>
+                    <Card.Text className="bookmark" onClick={() => bookmark(index)}>
+                        <img src={item.isBookmarked? bookmarkFull : bookmarkEmpty} alt="bookmark"/>
+                    </Card.Text>
+                </Card.ImgOverlay>
+                <Card.Body>
+                    <Card.Text className="card-info">
+                        <span className="info">
+                            <span>2019</span>
+                            <span>&#183;</span>
+                            <span className='catergory'>
+                                <img src={item.category === "Movie"? categoryMovie : categoryTV} alt="category"/> {item.category === "Movie"? "Movie" : "TV Series"}
+                            </span>
+                            <span>&#183;</span>
+                            <span>{item.rating}</span>
+                        </span>
+                        <span className="title">{item.title}</span>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+}
