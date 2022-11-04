aee97fab78d52c525582c2f35940b8a63b70148b
antran1245 <antran1245@gmail.com>
2022-11-04 16:20:30 -0700

    sign up check if password match repeat

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -2,44 +2,47 @@ import { useState } from "react"
 import { Button, Form, Row, Col } from "react-bootstrap"
 
 export default function SignUp({setLogin}) {
-    const [errors, setErrors] = useState({email: false, password: false, repeat: false})
+    const [errors, setErrors] = useState({email: false, password: false, repeat: ''})
     const [form, setForm] = useState({email: '', password: '', repeat: ''})
 
     const formFilled = (e) => {
         e.preventDefault()
-        setErrors({email: form.email === '', password: form.password === '', repeat: form.repeat === ''})
+        setErrors({email: form.email === '', password: form.password === '', repeat: form.repeat === ''? "Can't be empty": ''})
+        if(form.password !== form.repeat) {
+            setErrors({...errors, repeat: 'Password Not Match'})
+        }
     }
     return(
         <div className="form-box">
             <Form onSubmit={formFilled}>
                 <h2>Sign Up</h2>
                 <Form.Group className="form-group" as={Row}>
-                    <Col xs={'auto'} className={`p-0 ${!errors.email && 'w-100'}`}>
+                    <Col xs={6} className={`p-0 ${!errors.email && 'w-100'}`}>
                         <Form.Control type="text" placeholder="Email address" className="ps-1 pe-0 mt-0" value={form.email} onChange={(e) => setForm({...form, email: e.target.value})}/>
                     </Col>
                     {errors.email && 
-                    <Col xs={'auto'} className="d-flex justify-content-center align-items-center">
+                    <Col xs={6} className="d-flex justify-content-center align-items-center">
                         <Form.Text className="mt-0">Can't be empty</Form.Text>
                     </Col>
                     }
                 </Form.Group>
                 <Form.Group className="form-group" as={Row}>
-                    <Col xs={'auto'} className={`p-0 ${!errors.password && 'w-100'}`}>
+                    <Col xs={6} className={`p-0 ${!errors.password && 'w-100'}`}>
                         <Form.Control type="text" placeholder="Password" className="ps-1 pe-0 mt-0" value={form.password} onChange={(e) => setForm({...form, password: e.target.value})}/>
                     </Col>
                     {errors.password && 
-                    <Col xs={'auto'} className="d-flex justify-content-center align-items-center">
+                    <Col xs={6} className="d-flex justify-content-center align-items-center">
                         <Form.Text className="mt-0">Can't be empty</Form.Text>
                     </Col>
                     }
                 </Form.Group>
                 <Form.Group className="form-group" as={Row}>
-                    <Col xs={'auto'} className={`p-0 ${!errors.repeat && 'w-100'}`}>
+                    <Col xs={6} className={`p-0 ${errors.repeat === '' && 'w-100'}`}>
                         <Form.Control type="text" placeholder="Repeat Password" className="ps-1 pe-0 mt-0" value={form.repeat} onChange={(e) => setForm({...form, repeat: e.target.value})}/>
                     </Col>
-                    {errors.repeat && 
-                    <Col xs={'auto'} className="d-flex justify-content-center align-items-center">
-                        <Form.Text className="mt-0">Can't be empty</Form.Text>
+                    {errors.repeat !== '' && 
+                    <Col xs={6} className="d-flex justify-content-center align-items-center">
+                        <Form.Text className="mt-0">{errors.repeat}</Form.Text>
                     </Col>
                     }
                 </Form.Group>
